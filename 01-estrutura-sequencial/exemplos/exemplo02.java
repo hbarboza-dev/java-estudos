355eaaf1f583f60f1c4c7e2d5425920e60d0ce06
Harisson Gabriel <hbarboza.dev@gmail.com>
2026-05-22 21:40:12 -0300

    Adicionando novos exemplos do tipo Casting 2,3,4,5,6

---
+++ 01-estrutura-sequencial/exemplos/exemplo02.java
@@ -0,0 +1,14 @@
+package exemplos;
+
+public class exemplo02 {
+    public static void main (String[] args){
+        int x, y;
+
+        x = 5;
+
+        y = 2 * x;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
---
+++ 01-estrutura-sequencial/exemplos/exemplo06.java
@@ -0,0 +1,15 @@
+package exemplos;
+
+public class exemplo06 {
+    public static void main (String[] args){
+
+        double a;
+        int b;
+
+        a = 5.0;
+        b = (int) a;
+
+        System.out.println(b);
+
+    }
+}
